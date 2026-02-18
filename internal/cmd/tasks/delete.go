6377ff9b09ba4d552bb0aabb6597b266e85d3873
Rob Barry <rob.barry@wsj.com>
2026-02-18 10:28:37 -0500

    remove interactive prompt from tasks delete

--- internal/cmd/tasks/delete.go
+++ internal/cmd/tasks/delete.go
@@ -1,10 +1,7 @@
 package tasks
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 
 	"github.com/spf13/cobra"
 	"thingies/internal/cmd/shared"
@@ -12,18 +9,12 @@ import (
 	"thingies/internal/things"
 )
 
-var deleteForce bool
-
 var deleteCmd = &cobra.Command{
 	Use:   "delete <uuid>",
 	Short: "Delete a task",
 	Long:  `Delete (trash) a task using AppleScript.`,
 	Args:  cobra.ExactArgs(1),
 	RunE:  runDelete,
-}
-
-func init() {
-	deleteCmd.Flags().BoolVarP(&deleteForce, "force", "f", false, "Skip confirmation")
 }
 
 func runDelete(cmd *cobra.Command, args []string) error {
@@ -36,26 +27,6 @@ func runDelete(cmd *cobra.Command, args []string) error {
 	uuid, err := thingsDB.ResolveTaskUUID(args[0])
 	if err != nil {
 		return err
-	}
-
-	// Get task info for confirmation
-	if !deleteForce {
-		task, err := thingsDB.GetTask(uuid)
-		if err != nil {
-			return err
-		}
-
-		fmt.Printf("Delete task: %s\n", task.Title)
-		fmt.Print("Are you sure? [y/N] ")
-
-		reader := bufio.NewReader(os.Stdin)
-		response, _ := reader.ReadString('\n')
-		response = strings.TrimSpace(strings.ToLower(response))
-
-		if response != "y" && response != "yes" {
-			fmt.Println("Canceled")
-			return nil
-		}
 	}
 
 	if err := things.DeleteTask(uuid); err != nil {
